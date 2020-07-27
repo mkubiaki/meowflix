50f32c936b2d2c15381132667db4a4aad4000944
Marcelo Kubiaki <kubiaki.crfrs@gmail.com>
2020-07-27 17:19:27 -0300

    Acrescentado Jacarelvis e Bita na lista de vídeos

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Menu from './components/Menu';
 import dadosIniciais from './data/dados_iniciais.json';
 import BannerMain from './components/BannerMain';
 import Carousel from './components/Carousel';
-import Footer from './components/Footer';
+import FooterBase from './components/Footer';
 
 function App() {
   return (
@@ -24,18 +24,8 @@ function App() {
       <Carousel
         category={dadosIniciais.categorias[1]}
       />
-      <Carousel
-        category={dadosIniciais.categorias[2]}
-      />
-      <Carousel
-        category={dadosIniciais.categorias[3]}
-      />
-      <Carousel
-        category={dadosIniciais.categorias[4]}
-      />
-      <Carousel
-        category={dadosIniciais.categorias[5]}
-      />
+
+      <FooterBase />
     </div>
   );
 }
