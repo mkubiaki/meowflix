e7779011d5ed7d2f8b2cfaf064f2f41b5bf064f4
Marcelo Kubiaki <kubiaki.crfrs@gmail.com>
2020-07-30 22:02:40 -0300

    Aula 3 - Form de Cadastro e reutilizáveis

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,16 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
+import FormField from '../../../components/FormField';
 
 function CadastroCategoria(){
-    return (
-      <PageDefault>
-        <h1>Cadastro de Categoria</h1>
-        <Link to="/cadastro/video">
-            Cadastrar Vídeo
-        </Link>
-      </PageDefault>
-    )
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: '',
+  }
+  const [categorias, setCategorias] = useState([]);
+  const [values, setValues] = useState(valoresIniciais);
+
+  function setValue(chave, valor) {
+    setValues({
+      ...values,
+      [chave]: valor,
+    })
+  }
+
+  function handleChange(infosDoEvento){
+    setValue(
+      infosDoEvento.target.getAttribute('name'),
+      infosDoEvento.target.value
+    );
+  }
+
+  return (
+    <PageDefault>
+      <h1>Cadastro de Categoria: {values.nome}</h1>
+
+      <form onSubmit={
+          function handleSubmit(infosDoEvento){
+            infosDoEvento.preventDefault();
+            setCategorias([
+              ...categorias,
+              values
+            ]);
+        
+            setValues(valoresIniciais);
+          }
+      }>
+        <FormField
+          type='text'
+          label='Nome da categoria :'
+          name='nome'
+          value={values.nome}
+          onChange={handleChange}
+        />
+        <FormField
+          type='text'
+          label='Descrição da categoria :'
+          name='descricao'
+          value={values.descricao}
+          onChange={handleChange}
+        />
+        <FormField
+          type='color'
+          label='Cor :'
+          name='cor'
+          value={values.cor}
+          onChange={handleChange}
+        />
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
+
+      <ul>
+        {categorias.map((categoria, indice) => {
+          return (
+            <li key={`${categoria}${indice}`}>
+              {categoria.nome} - {categoria.descricao} - {categoria.cor}
+            </li>
+          )
+        })}
+      </ul>
+      <Link to="/cadastro/video">
+          Cadastrar Vídeo
+      </Link>
+    </PageDefault>
+  )
 }
 
 export default CadastroCategoria;
