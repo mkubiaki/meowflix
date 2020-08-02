40326c2e49aa0b0c77a4c0a91b9a6434491602d5
Marcelo Kubiaki <kubiaki.crfrs@gmail.com>
2020-08-02 20:45:01 -0300

    Aula 4 - Validação e Backend na Heroku :)

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,33 +1,34 @@
 import React from 'react';
 import Menu from '../../components/Menu';
+//import dadosIniciais from 'http://localhost:3001/categorias';
 import dadosIniciais from '../../data/dados_iniciais.json';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
 import FooterBase from '../../components/Footer';
 
 function Home() {
-  return (
-    <div style={{ background: "#141414" }}>
-      <Menu />
+	return (
+		<div style={{ background: '#141414' }}>
+			<Menu />
 
-      <BannerMain
-        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
-        url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"O que é Front-End? Trabalhando na área"}
-      />
+			<BannerMain
+				videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+				url={dadosIniciais.categorias[0].videos[0].url}
+				videoDescription={'O que é Front-End? Trabalhando na área'}
+			/>
 
-      <Carousel
-        ignoreFirstVideo
-        category={dadosIniciais.categorias[0]}
-      />
+			<Carousel
+				ignoreFirstVideo
+				category={dadosIniciais.categorias[0]}
+			/>
 
-      <Carousel
-        category={dadosIniciais.categorias[1]}
-      />
+			<Carousel
+				category={dadosIniciais.categorias[1]}
+			/>
 
-      <FooterBase />
-    </div>
-  );
+			<FooterBase />
+		</div>
+	);
 }
 
 export default Home;
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,87 +1,106 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PageDefault from '../../../components/PageDefault';
+import Button from '../../../components/Button';
 import { Link } from 'react-router-dom';
 import FormField from '../../../components/FormField';
 
 function CadastroCategoria(){
-  const valoresIniciais = {
-    nome: '',
-    descricao: '',
-    cor: '',
-  }
-  const [categorias, setCategorias] = useState([]);
-  const [values, setValues] = useState(valoresIniciais);
+	const valoresIniciais = {
+		nome: '',
+		descricao: '',
+		cor: '',
+	};
+	const [categorias, setCategorias] = useState([]);
+	const [values, setValues] = useState(valoresIniciais);
 
-  function setValue(chave, valor) {
-    setValues({
-      ...values,
-      [chave]: valor,
-    })
-  }
+	function setValue(chave, valor) {
+		setValues({
+			...values,
+			[chave]: valor,
+		});
+	}
 
-  function handleChange(infosDoEvento){
-    setValue(
-      infosDoEvento.target.getAttribute('name'),
-      infosDoEvento.target.value
-    );
-  }
+	function handleChange(infosDoEvento){
+		setValue(
+			infosDoEvento.target.getAttribute('name'),
+			infosDoEvento.target.value
+		);
+	}
 
-  return (
-    <PageDefault>
-      <h1>Cadastro de Categoria: {values.nome}</h1>
+	useEffect(() => {
+		console.log('alo');
+		const URL = 'http://localhost:3001/categorias';
+		fetch(URL)
+		.then(async (resposta) => {
+			const objeto = await resposta.json();
+			setCategorias([
+				...objeto,
+			]);
+		});
+	}, []);
 
-      <form onSubmit={
-          function handleSubmit(infosDoEvento){
-            infosDoEvento.preventDefault();
-            setCategorias([
-              ...categorias,
-              values
-            ]);
+	return (
+		<PageDefault>
+			<h1>Cadastro de Categoria: {values.nome}</h1>
+
+			<form onSubmit={
+				function handleSubmit(infosDoEvento){
+					infosDoEvento.preventDefault();
+					setCategorias([
+						...categorias,
+						values
+					]);
         
-            setValues(valoresIniciais);
-          }
-      }>
-        <FormField
-          type='text'
-          label='Nome da categoria :'
-          name='nome'
-          value={values.nome}
-          onChange={handleChange}
-        />
-        <FormField
-          type='text'
-          label='Descrição da categoria :'
-          name='descricao'
-          value={values.descricao}
-          onChange={handleChange}
-        />
-        <FormField
-          type='color'
-          label='Cor :'
-          name='cor'
-          value={values.cor}
-          onChange={handleChange}
-        />
+					setValues(valoresIniciais);
+				}
+			}>
+				<FormField
+					type='text'
+					label='Nome da categoria'
+					name='nome'
+					value={values.nome}
+					onChange={handleChange}
+				/>
+				<FormField
+					type='textarea'
+					label='Descrição da categoria'
+					name='descricao'
+					value={values.descricao}
+					onChange={handleChange}
+				/>
+				<FormField
+					type='color'
+					label='Cor'
+					name='cor'
+					value={values.cor}
+					onChange={handleChange}
+				/>
 
-        <button>
+				<Button>
           Cadastrar
-        </button>
-      </form>
+				</Button>
+			</form>
 
-      <ul>
-        {categorias.map((categoria, indice) => {
-          return (
-            <li key={`${categoria}${indice}`}>
-              {categoria.nome} - {categoria.descricao} - {categoria.cor}
-            </li>
-          )
-        })}
-      </ul>
-      <Link to="/cadastro/video">
+			{categorias.length === 0 && (
+				<div>
+					Loading...
+				</div>
+			)}
+
+			<ul>
+				{categorias.map((categoria, indice) => {
+					return (
+						<li key={`${categoria}${indice}`}>
+							{categoria.nome} - {categoria.descricao} - {categoria.cor}
+						</li>
+					);
+				})}
+			</ul>
+			<Link to="/cadastro/video">
           Cadastrar Vídeo
-      </Link>
-    </PageDefault>
-  )
+			</Link>
+		</PageDefault>
+	);
 }
 
 export default CadastroCategoria;
